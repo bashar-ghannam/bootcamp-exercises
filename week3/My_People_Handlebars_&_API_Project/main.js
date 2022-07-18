1a82c52dbb89d2d6306a8e0aa12de9fd80b985d7
bashar-ghannam <78476457+bashar-ghannam@users.noreply.github.com>
2022-07-19 01:25:01 +0300

    Add My People - Handlebars & API Project final version

--- week3/My_People_Handlebars_&_API_Project/main.js
+++ week3/My_People_Handlebars_&_API_Project/main.js
@@ -1,23 +1,16 @@
 const render = function (results) {
   const source = $('#MyPeople-template').html();
   const template = Handlebars.compile(source);
-  let newHTML = template({ results });
+  let newHTML = template(results);
   $('.grid-container').empty();
   $('.grid-container').append(newHTML);
 };
 
 $.ajax({
   method: 'GET',
-  url: 'https://randomuser.me/api/?results=5',
+  url: 'https://randomuser.me/api/?results=10',
   success: render,
   error: function (xhr, text, error) {
     console.log(text);
   },
-});
-
-$('.grid-container').on('mouseover', '.grid-item', function () {
-  $(this).css('background-color', 'rgb(63, 141, 211)');
-  setTimeout(function () {
-    $('.grid-item').css('background-color', 'rgba(212, 202, 202, 0.8)');
-  }, 1000);
 });
