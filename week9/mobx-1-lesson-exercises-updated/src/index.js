e58aba4a9ac85d1a7a3d37bc9ee7820cd1bc1598
bashar-ghannam <78476457+bashar-ghannam@users.noreply.github.com>
2022-09-11 17:46:59 +0300

    final commit

--- week9/mobx-1-lesson-exercises-updated/src/index.js
+++ week9/mobx-1-lesson-exercises-updated/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { Item } from '../src/stores/Item';
 import { ShoppingList } from '../src/stores/ShoppingList';
 
 
